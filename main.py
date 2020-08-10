b2f1e92f90180294138374eb081b2a9660d214e5
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-08-11 00:37:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import datetime
 
 class Current_time(object):
     def __init__(self):
-        self.date = str(datetime.datetime.fromtimestamp\
-            (time.time()).strftime('%d-%m-%Y %H:%M:%S'))
+        self.date = str(datetime.datetime.fromtimestamp\              
+            (time.time()).strftime('%d-%m-%Y %H:%M:%S'))     # first way
     def __repr__(self):
         return self.date
 
 if __name__ == "__main__":
     print(Current_time())
+    print(datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S'))       # second way
